baebb3e0ecef96eb810719c77b61cba6e495a6a8
Sam Merrell <merrell.sam@gmail.com>
2012-07-13 17:00:07 -0600

    Parsing XML response from YouTrack
    Checking the type of issue as well as a custom field Approved For Work.

--- require_youtrack_issue.rb
+++ require_youtrack_issue.rb
@@ -3,12 +3,12 @@
 require 'net/http'
 require 'nokogiri'
 
-server_url = 'youtrack.server.com'
+server_url = ARGV
 
 text = '''
 This is a test comment that should look for youtrack #yt-123
 issues. It should find all of them and check them against
-the #xx-321 youtrack #ab-789 server #cd-581
+the #xx-321 youtrack #ab-789 server #ms-581
 '''
 
 puts "issues from text"
@@ -17,7 +17,7 @@ puts
 
 issue_regex = /( |^)#(\w+-\d+)/
 
-issues = text.scan(issue_regex) { |m, issue|
+text.scan(issue_regex) { |m, issue|
   http = Net::HTTP.new(server_url)
 
   exists_url = "/rest/issue/#{issue}"
@@ -26,4 +26,13 @@ issues = text.scan(issue_regex) { |m, issue|
 
   puts "Issue not found: #{issue}" if response.code == '404'
   puts "Issue found: #{issue}" unless response.code != '200'
+
+  if response.code == '200'
+    xml = Nokogiri::XML(response.body)
+    type = xml.xpath('//field[@name = "Type"]/value/text()')
+    approved = xml.xpath('//field[@name = "Approved For Work"]/value/text()')
+    puts xml
+    puts "Type: #{type}"
+    puts "Approved: #{approved}"
+  end
 }
